38b22b46b1ee63d3afd266b46822277390b34f38
tangramor <tangramor@gmail.com>
2023-02-24 11:40:25 +0800

    add run_ssl_svn_alma8.sh

---
+++ run_ssl_svn_alma8.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker run -d -it \
+    -p 3690:3690 \
+    -p 443:443 \
+    -p 80:80 \
+    -v /etc/localtime:/etc/localtime \
+    -v ${PWD}/svnadmin:/home/svnadmin \
+    --name svn \
+    tangramor/svn_ssl_admin:alma8
