2d92c081d739506349b572eb36cebddb8200619d
tangramor <tangramor@gmail.com>
2024-07-10 23:21:06 +0800

    use dynamici18n branch and fix centos7 repo bug

--- build.alma9.sh
+++ build.alma9.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t tangramor/svn_ssl_admin:alma9 -f Dockerfile.alma9 .
+docker build --no-cache -t tangramor/svn_ssl_admin:alma9 -f Dockerfile.alma9 .
 
 docker tag tangramor/svn_ssl_admin:alma9 tangramor/svn_ssl_admin:latest
